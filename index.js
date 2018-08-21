dc7609df19b403026879d51f7e0f603736899fee
brynn.shepherd@gmail.com <brynn.shepherd@gmail.com>
2018-08-21 00:22:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var whatTheyPlay = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var whatHePlays = musicians[i] + " plays " + instruments[i];
+    whatTheyPlay[i] = whatHePlays;
+  }
+  return whatTheyPlay;
+}
+
+function johnLennonFacts(facts) {
+  
+  
+}
